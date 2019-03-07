6380007a0ef301569fbe8f29295a6b78f95ac129
patrick_xtn <rapaelec@gmail.com>
2019-03-07 19:14:46 +0300

    add JWT in api and adminer

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HydraAdmin } from '@api-platform/admin';
+import authProvider from './src/authProvider';
+
 
 const entrypoint = document.getElementById('api-entrypoint').innerText;
 
-ReactDOM.render(<HydraAdmin entrypoint={entrypoint}/>, document.getElementById('api-platform-admin'));
+ReactDOM.render(<HydraAdmin authProvider={authProvider} entrypoint={entrypoint}/>, document.getElementById('api-platform-admin'));
+
